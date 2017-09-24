536eae49035d5376621b093a664129066d200ddd
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2017-09-24 19:06:55 -0400

    Uppin' the timeout because I'm cheap.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Client::class, function () {
             return new Client([
                 'base_uri' => env('ANDRAE_API_URL'),
-                'timeout' => 10
+                'timeout' => 15
             ]);
         });
         $this->app->bind(PostRepositoryInterface::class, PostRepository::class);
